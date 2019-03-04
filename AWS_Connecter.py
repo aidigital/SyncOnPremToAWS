ec197eea77642f136d136b47833ad17445b55aa6
Adrian Iordache <adrianiordache1991@gmail.com>
2019-03-04 12:31:11 +0000

    tables going to Dev reading also from LIVE

--- AWS_Connecter.py
+++ AWS_Connecter.py
@@ -299,9 +299,7 @@ if __name__ == "__main__":
     #AWS.execute_sql(sql_statement='SELECT COUNT(*) FROM SA_RESIDENTS')  # smoke test
     #print('successful connection to AWS Oracle')
 
-    AWS.execute_sql(sql_statement='DELETE FROM SA_RESIDENTS')
-    AWS.execute_sql(sql_statement='DELETE FROM GD_RESIDENTS')
-
+    # AWS.execute_sql(sql_statement='DELETE FROM SA_RESIDENTS')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_LOOKUP')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_PATCH_LOOKUP')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_TENURE_TYPE')
@@ -312,10 +310,9 @@ if __name__ == "__main__":
     #
     # AWS.execute_sql(sql_statement='DELETE FROM SA_BLOCKS')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_SUB_BLOCKS')
-    # AWS.execute_sql(sql_statement='DELETE FROM SA_UNITS')  # this deletes only the data
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_UNITS')  # careful, this is GD
-
-
+    # AWS.execute_sql(sql_statement='DELETE FROM SA_UNITS')
+    #
+    #
     # AWS.execute_sql(sql_statement='DELETE FROM SA_RESIDENTS')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_PERSON')
     #
@@ -326,16 +323,6 @@ if __name__ == "__main__":
     # AWS.execute_sql(sql_statement='DELETE FROM SA_ECONOMIC_STATUS')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_VULNERABILTY_DETAILS')
     #
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_RESIDENTS')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_RENT_GRP_REF')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_PERSON')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_PERSON_LOOKUP')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_CONTACT_PREFRENCES')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_COMMUNICATION')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_ECONOMIC_STATUS')
-    # AWS.execute_sql(sql_statement='DELETE FROM GD_VULNERABILTY_DETAILS')
-    # #AWS.execute_sql(sql_statement='DROP TABLE Dummy_Table PURGE')  # this deletes the table completely
-
     # AWS.execute_sql(sql_statement='DELETE FROM SA_ESTATE_INSP_AND_CLEANING')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_PROPERTY_TYPE')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_UNIT_CLUSTER')
@@ -344,7 +331,7 @@ if __name__ == "__main__":
     # AWS.execute_sql(sql_statement='DELETE FROM SA_ATTRIBUTE_KEY_LOOKUP')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_PROP_ATTRIBUTE_TYPE')
     # AWS.execute_sql(sql_statement='DELETE FROM SA_PROP_ATTRIBUTE_KEY_VALUE')
-    #
+
     # AWS.execute_sql(sql_statement='DELETE FROM GD_UNITS')  # this deletes only the data
     # AWS.execute_sql(sql_statement='DELETE FROM GD_SCHEMES')
     # AWS.execute_sql(sql_statement='DELETE FROM GD_BLOCKS')
